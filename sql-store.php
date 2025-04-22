d5c1a06fc9fa1307263b79262a5a18c885b3b36b
Edgars G <Edgars@gmail>
2025-04-22 14:42:49 +0300

    atrgriež datus no customers tabulas un tos saliek listā

--- sql-store.php
+++ sql-store.php
@@ -9,4 +9,15 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+$sql = "SELECT * from customers";
+$result = $conn->query($sql);
+echo "<ul>";
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo "<li> id: " . $row["customer_id"] . " - Name: " . $row["first_name"] . " " . $row["last_name"] . "<br>";
+    }
+}
+echo "<ul>";
+$conn->close();
